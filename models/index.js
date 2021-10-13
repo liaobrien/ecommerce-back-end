6e4f61096f4538981cf0a4d096dbb0021df2f36a
Lia OBrien <liaobrien123@gmail.com>
2021-10-13 19:30:20 -0400

    added category hasMany products

--- models/index.js
+++ models/index.js
@@ -7,10 +7,13 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE', s
 });
 
 // Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
 
