a4bf92e8e24db2a3e050588a24d024eec8fede7a
Lia OBrien <liaobrien123@gmail.com>
2021-10-13 19:28:53 -0400

    filled out product/category association

--- models/index.js
+++ models/index.js
@@ -5,6 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', s
+});
 
 // Categories have many Products
 
